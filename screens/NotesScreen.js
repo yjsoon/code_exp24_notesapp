00a1091e30b38eba5ea719a86c3031da87087ef1
YJ Soon <yjsoon@tinkertanker.com>
2024-05-21 14:58:17 +0800

    Added a button to the top right

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -1,8 +1,23 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { useEffect } from "react";
+import { Entypo } from "@expo/vector-icons";
 
 export default function NotesScreen({ navigation }) {
-  useEffect(() => {});
+  function addNote() {
+    console.log("Adding note");
+  }
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Pressable onPress={addNote}>
+            <Entypo name="new-message" size={24} color="black" />
+          </Pressable>
+        );
+      }
+    });
+  });
 
   return (
     <View style={styles.container}>
