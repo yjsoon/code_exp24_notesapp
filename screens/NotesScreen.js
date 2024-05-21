ed4b089e6a354ffdbc36422c2d6317d0abf52857
YJ Soon <yjsoon@tinkertanker.com>
2024-05-21 15:06:35 +0800

    Added a flat list with sample notes

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -1,10 +1,19 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import { useEffect } from "react";
+import { View, Text, StyleSheet, Pressable, FlatList } from "react-native";
+import { useState, useEffect } from "react";
 import { Entypo } from "@expo/vector-icons";
 
 export default function NotesScreen({ navigation }) {
+  const [notes, setNotes] = useState([
+    { id: "1", title: "Walk the dog every day" },
+    { id: "2", title: "Some thoughts about useEffect and its various modes" }
+  ]);
+
   function addNote() {
-    console.log("Adding note");
+    let newNote = {
+      id: `${notes.length + 1}`,
+      title: "Sample new note that's added"
+    };
+    setNotes([...notes, newNote]);
   }
 
   useEffect(() => {
@@ -19,9 +28,33 @@ export default function NotesScreen({ navigation }) {
     });
   });
 
+  function renderNote({ item }) {
+    return (
+      <View
+        style={{
+          padding: 12,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#aaa",
+          borderBottomWidth: 1
+        }}>
+        <Text
+          style={{
+            fontSize: 20
+          }}>
+          {item.title}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Notes</Text>
+      <FlatList
+        style={{ width: "100%" }}
+        data={notes}
+        renderItem={renderNote}
+      />
     </View>
   );
 }
