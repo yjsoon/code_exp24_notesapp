3ea1c6267e5bd4e4ac7330c3b86600b0dec47417
YJ Soon <yjsoon@tinkertanker.com>
2024-05-21 15:45:56 +0800

    Styled and added views for AddNoteScreen

--- screens/AddNoteScreen.js
+++ screens/AddNoteScreen.js
@@ -1,9 +1,29 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function AddNoteScreen() {
+  const [title, onChangeTitle] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>AddNoteScreen</Text>
+      <Text style={styles.label}>Add your note here</Text>
+      <TextInput
+        style={styles.textInput}
+        value={title}
+        onChangeText={(newText) => onChangeTitle(newText)}
+      />
+      <View style={styles.buttons}>
+        <Pressable
+          onPress={() => {}}
+          style={[styles.button, styles.submitButton]}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {}}
+          style={[styles.button, styles.cancelButton]}>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -14,5 +34,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 24
+  },
+  textInput: {
+    margin: 20,
+    padding: 10,
+    borderWidth: 1,
+    width: "80%",
+    borderColor: "#ccc"
+  },
+  buttons: {
+    flexDirection: "row",
+    gap: 20
+  },
+  button: {
+    padding: 20,
+    borderRadius: 5
+  },
+  submitButton: {
+    backgroundColor: "green"
+  },
+  cancelButton: {
+    backgroundColor: "red"
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 24
   }
 });
